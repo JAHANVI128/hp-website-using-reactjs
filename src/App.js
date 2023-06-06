60b88fca86b0a2f40d22142f2c7f35f6620fb775
Jahanvi <jahanvimp128@gmail.com>
2023-06-06 14:28:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -3,20 +3,45 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+      <div class="navbar">
+        {/* <img src="" alt="Heena's pickle logo"> */}
+        <h1>Navbar</h1>
+    </div>
+
+    <div class="product">
+        <h1>Our Products</h1>
+    </div>
+
+    <div class="product">
+        <h1>How to Choose Flavours?</h1>
+    </div>
+    
+    <div class="choose">
+        <h2>Why Choose Us</h2>
+    </div>
+    
+    <div class="about">
+        <h1>About Us</h1>
+    </div>
+
+    <div class="text-center">
+        <h2>Customer Favourites</h2>
+    </div>
+
+    <div class="text-color">
+        <h1>Types of Indian Pickles You Must Try</h1>
+    </div>
+
+    <div class="navbar footer">
+        {/* <img src="" alt="Heena's pickle logo"> */}
+        <h4>About Us</h4>
+    </div>
+
+    <footer class="footer">
+         © Copyright 2023. All Rights Reserved.
+    </footer>
       </header>
     </div>
   );
